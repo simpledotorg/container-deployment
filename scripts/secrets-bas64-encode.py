946f4b3ce6304c366ddeaac48c4a337b8bd4b507
roypeter <>
2022-09-01 17:36:53 +0530

    Handle int keys in encode script

--- scripts/secrets-bas64-encode.py
+++ scripts/secrets-bas64-encode.py
@@ -5,7 +5,11 @@ with open("../k8s/argocd-apps/bd-k3s-demo/secrets/simple-server.sealedsecret.yam
   try:
     parsed_yaml=yaml.safe_load(stream)
     for key, value in parsed_yaml['data'].items():
-      base64_string = base64.b64encode(value.encode('utf-8'))
-      print("%s: \"%s\"" % (key, base64_string.decode('utf-8')))
+      if value is not None:
+        base64_string = base64.b64encode(str(value).encode('utf-8')).decode('utf-8')
+      else:
+        base64_string = ""
+        
+      print("%s: \"%s\"" % (key, base64_string))
   except yaml.YAMLError as exc:
     print(exc)
