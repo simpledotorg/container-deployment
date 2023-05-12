0e72942f8bb56d8f13f44d22b9e010e299acf4f3
roy peter <roypeter@W5V6DYL96K-rpeter.local>
2023-05-12 17:18:26 +0530

    Fix tf s3 module variable type

--- terraform/modules/simple_s3/variables.tf
+++ terraform/modules/simple_s3/variables.tf
@@ -5,7 +5,7 @@ variable "bucket_name" {
 # S3 actions allowed for the user for this bucket
 # S3 actions: https://docs.aws.amazon.com/AmazonS3/latest/dev/using-with-s3-actions.html
 variable "allowed_actions" {
-  type = array(string)
+  type = list(string)
   default = [
     "s3:*" # Allow all actions
   ]
