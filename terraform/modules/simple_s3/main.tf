1bf8fe630354044d5727656bd3530ab4f0a74ee6
roy peter <roypeter@W5V6DYL96K-rpeter.local>
2023-05-12 17:14:47 +0530

    Update tf s3 module comments

--- terraform/modules/simple_s3/main.tf
+++ terraform/modules/simple_s3/main.tf
@@ -1,4 +1,3 @@
-# Create s3 bucket
 resource "aws_s3_bucket" "simple_s3" {
   bucket = var.bucket_name
   acl    = "private"
@@ -11,12 +10,10 @@ resource "aws_s3_bucket" "simple_s3" {
   )
 }
 
-# Create a iam user
 resource "aws_iam_user" "simple_s3_user" {
   name = var.bucket_name
 }
 
-# Create a iam policy document
 data "aws_iam_policy_document" "simple_s3_policy" {
   statement {
     actions   = var.allowed_actions
@@ -25,20 +22,17 @@ data "aws_iam_policy_document" "simple_s3_policy" {
   }
 }
 
-# Create a iam policy to allow the user to access the bucket
 resource "aws_iam_policy" "simple_s3_policy" {
   name        = var.bucket_name
   description = "Allow access to simple-s3-bucket"
   policy      = data.aws_iam_policy_document.simple_s3_policy.json
 }
 
-# Attach the policy to the user
 resource "aws_iam_user_policy_attachment" "simple_s3_user_policy" {
   user       = aws_iam_user.simple_s3_user.name
   policy_arn = aws_iam_policy.simple_s3_policy.arn
 }
 
-# Generate the access key and secret key for the user
 resource "aws_iam_access_key" "simple_s3_user_access_key" {
   user = aws_iam_user.simple_s3_user.name
 }
--- terraform/modules/simple_s3/variables.tf
+++ terraform/modules/simple_s3/variables.tf
@@ -2,11 +2,12 @@ variable "bucket_name" {
   type = string
 }
 
+# S3 actions allowed for the user for this bucket
 # S3 actions: https://docs.aws.amazon.com/AmazonS3/latest/dev/using-with-s3-actions.html
 variable "allowed_actions" {
   type = array(string)
   default = [
-    "s3:*"
+    "s3:*" # Allow all actions
   ]
 }
 
