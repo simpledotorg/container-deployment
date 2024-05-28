b1409e5c78ad39c955dea4c1ca96cefdefa383b2
Roy Peter <16620459+roypeter@users.noreply.github.com>
2024-05-28 11:31:20 +0530

    DHIS2 epidemics sandbox (#142)
    **Story card:**
    [sc-12635](https://app.shortcut.com/simpledotorg/story/12635)

--- terraform/sandbox/main.tf
+++ terraform/sandbox/main.tf
@@ -123,6 +123,17 @@ module "eks" {
         role-dhis2-sandbox-01 = "true"
       }
       instance_types = ["t3a.2xlarge"]
+    },
+    {
+      name         = "dhis2-sandbox-epd"
+      create       = true
+      min_size     = 1
+      max_size     = 1
+      desired_size = 1
+      labels = {
+        role-dhis2-sandbox-epidemics = "true"
+      }
+      instance_types = ["t3a.2xlarge"]
     }
   ]
 }
