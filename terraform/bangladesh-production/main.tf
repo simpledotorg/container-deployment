a7d7cfa55416e6cdee13d9144ece8228583f6b57
sshinde-rtsl <sshinde@rtsl.org>
2024-12-12 09:57:51 +0530

    Sagar shinde/sc 13617 (#251)
    **Story card:**
    [sc-13617](https://app.shortcut.com/simpledotorg/story/13617/deploy-loki-to-bangladesh-production)

--- terraform/bangladesh-production/main.tf
+++ terraform/bangladesh-production/main.tf
@@ -130,6 +130,13 @@ module "db_backup_s3_bucket" {
   allowed_ips = module.vpc.nat_public_ips
 }
 
+module "loki_s3_bucket" {
+  source      = "../modules/simple_s3"
+  bucket_name = "rtsl-bangladesh-production-loki"
+  tags        = local.tags
+  allowed_ips = module.vpc.nat_public_ips
+}
+
 # Log archival bucket and user is reused from old environment
 # https://github.com/simpledotorg/deployment/blob/master/terraform/bangladesh/main.tf
 
@@ -181,3 +188,25 @@ output "db_backup_s3_access_secret" {
   value     = module.db_backup_s3_bucket.access_secret
   sensitive = true
 }
+
+output "loki_s3_bucket_id" {
+  value = module.loki_s3_bucket.bucket_id
+}
+
+output "loki_s3_bucket_arn" {
+  value = module.loki_s3_bucket.bucket_arn
+}
+
+output "loki_s3_user_arn" {
+  value = module.loki_s3_bucket.bucket_arn
+}
+
+output "loki_s3_access_key" {
+  value = module.loki_s3_bucket.access_key
+}
+
+output "loki_s3_access_secret" {
+  value     = module.loki_s3_bucket.access_secret
+  sensitive = true
+}
+
